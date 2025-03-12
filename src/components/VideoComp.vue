581de0a6098932c73861415d51cf887220fc59f6
Marco Milza <76559736+182Marco@users.noreply.github.com>
2025-03-12 12:24:53 +0000

    better centering wrapped you tube video

--- src/components/VideoComp.vue
+++ src/components/VideoComp.vue
@@ -11,68 +11,71 @@
       frameborder="0"
       allowfullscreen
       allow="autoplay"
-    >
-      ref="video" ></iframe
-    >
+    ></iframe>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'VideoComp',
-    props: {
-      keyFromApi: String,
-      obj: Object,
-    },
-    components: {},
-    data() {
-      return {
-        renderVideo: true,
-        playWhenCreated: '?autoplay=1',
-        showXclose: false,
-        // dal 2018 you tube non fa più bloccare i related
-        // però si possono bloccare almeno quelli sotto
-        //  il video quando si preme stop. Non lo uso perchè
-        // sembra esssere alternativo all'autoplay
-        noShowReletedWhenStop: '?rel=0',
-      };
-    },
-    created() {
-      /* La X per la chiusura video viene renderizzata prima che video sia caricato ->
-          sovrapponendosi alla X per la chiusura della preview del film quindi timeOut per rendering*/
-      setTimeout(() => (this.showXclose = true), 700);
-    },
-    methods: {},
-  };
+export default {
+  name: "VideoComp",
+  props: {
+    keyFromApi: String,
+    obj: Object,
+  },
+  data() {
+    return {
+      renderVideo: true,
+      playWhenCreated: "?autoplay=1",
+      showXclose: false,
+      noShowReletedWhenStop: "?rel=0",
+    };
+  },
+  created() {
+    // The close icon (X) is rendered before the video loads,
+    // so we delay its appearance by 700ms to avoid overlapping with the preview's close icon.
+    setTimeout(() => (this.showXclose = true), 700);
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- 
+  SCSS Styles:
+  .wrap is a fixed container that covers the entire viewport,
+  providing a semi-transparent background to highlight the video.
+  .x.close is the close icon positioned at 5% from the top and left of the container.
+  The iframe fills the entire container.
+-->
+
 <style scoped lang="scss">
-  /* parcials */
-  @import '@/scss/var';
-  @import '@/scss/reset';
-  @import '@/scss/mixins';
-  .wrap {
-    position: relative;
-    @include width-height(100vw, 100vh);
-    position: absolute;
-    left: -34px;
-    top: -250px;
-    z-index: 50;
-    .x.close {
-      font-size: $FontIco;
-      position: absolute;
-      left: 3%;
-      top: 15%;
-      color: $titleOfGrupsCol;
-      font-size: $FontIco;
-      cursor: pointer;
-      &:hover {
-        color: $white;
-      }
-    }
-    iframe {
-      @include width-height(100%, 100%);
-    }
-  }
+@import "@/scss/var";
+@import "@/scss/reset";
+@import "@/scss/mixins";
+
+.wrap {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: 50;
+  background-color: rgba(0, 0, 0, 0.7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.x.close {
+  font-size: 2rem;
+  color: white;
+  position: absolute;
+  top: 5%;
+  left: 5%;
+  cursor: pointer;
+  z-index: 100;
+}
+
+iframe {
+  width: 100%;
+  height: 100%;
+}
 </style>
