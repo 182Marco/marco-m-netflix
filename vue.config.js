344487daea001b63f2a442b626f74dcb9236db1d
Marco Milza <76559736+182Marco@users.noreply.github.com>
2025-03-12 11:54:28 +0000

    updated dependencies

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
-// vue.config.js
-const { defineConfig } = require("@vue/cli-service");
-
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/vue-boolflix/" : "/",
+  devServer: {
+    host: "0.0.0.0",
+    allowedHosts: ["all"],
+    disableHostCheck: true,
+  },
 };
